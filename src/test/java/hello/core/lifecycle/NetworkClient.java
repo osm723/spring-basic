3af610131ec94275d85e36e9ddc15cc0a8594930
Oh <osm723@gmail.com>
2024-08-15 11:38:36 +0900

    스프링 핵심 원리 - 기본편 섹션 8. 빈 생명주기 콜백 애노테이션 @PostConstruct, @PreDestroy

---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,66 @@
+package hello.core.lifecycle;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient //implements InitializingBean, DisposableBean
+{
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출 = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void connect() {
+        System.out.println("connect = " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call = " + url + ", message = " + message);
+    }
+
+    public void disconnect() {
+        System.out.println("close = " + url);
+    }
+
+    @PostConstruct
+    public void init() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        disconnect();
+    }
+
+//    public void init() throws Exception {
+//        System.out.println("NetworkClient.afterPropertiesSet");
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
+//
+//    public void close() throws Exception {
+//        disconnect();
+//    }
+
+//    @Override
+//    public void afterPropertiesSet() throws Exception {
+//        System.out.println("NetworkClient.afterPropertiesSet");
+//        connect();
+//        call("초기화 연결 메시지");
+//    }
+//
+//    @Override
+//    public void destroy() throws Exception {
+//        disconnect();
+//    }
+}
